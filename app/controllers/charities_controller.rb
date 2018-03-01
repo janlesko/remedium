9875061336df063f0b073bfdd9559ddca1bc83a0
Jan Lesko <jan.lesko.sk@gmail.com>
2018-03-01 11:15:33 +0800

    add mailer

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -1,7 +1,15 @@
 class CharitiesController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update]
+
   def index
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do
-  get 'sender_addresses/index'
-
-  get 'transactions/index'
-
-  get 'transactions/new'
-
-  get 'charities/index'
-
-  get 'charities/show'
-
   devise_for :users
   root to: 'charities#index'
-  resources :charities, only: [:index, :show] do
-    resources :transactions, only: [:new, :create, :index]
+  resources :charities, only: [:index, :show, :edit, :update] do
+    resources :transactions, only: [:index, :new, :create]
   end
   resources :sender_address, only: [:index]
 end
