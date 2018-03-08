b7559c145655617e6b661eb9aa1497eb1224661a
Philipp Preuss <philipp.arian@gmail.com>
2018-03-08 14:24:51 +0800

    work with inou

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
   mount Attachinary::Engine => "/attachinary"
   devise_for :users
-  root to: 'charities#index'
+  get "test", to: "pages#test"
+  root to: "pages#home"
+
   resources :charities, only: [:index, :show, :edit, :update] do
     resources :transactions, only: [:index, :new, :create]
     resources :items, only: [:create]
